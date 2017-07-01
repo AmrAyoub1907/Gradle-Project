33da2b2c45bb7731e22528ad857060ac714852a6
Amr Ayman Ayoub <amrayman1907@gmail.com>
2017-07-02 01:26:53 +0200

    Working

--- androidlib/src/main/java/com/amrayoub/androidlib/AndriodLibActivity.java
+++ androidlib/src/main/java/com/amrayoub/androidlib/AndriodLibActivity.java
@@ -3,13 +3,14 @@ package com.amrayoub.androidlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AndriodLibActivity extends AppCompatActivity {
     public final static String INTENT_JOKE = "INTENT_JOKE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lib);
         String s= getIntent().getStringExtra(INTENT_JOKE);
         TextView textView = (TextView) findViewById(R.id.joke);
         textView.setText(s);
